bc5e2b2330995f0c832ae0f48918f7c8aff31aea
xeac <45033391+xeac@users.noreply.github.com>
2020-05-19 17:33:04 +0300

    Create vpn.sh

---
+++ vpn.sh
@@ -0,0 +1,9 @@
+# install OpenVPN client on Ubuntu host;
+# login to ubuntu server;
+
+sudo su
+# apt-get install openvpn unzip
+# cd /tmp && wget https://files.ovpn.com/ubuntu_cli/ovpn-ro-bucharest.zip && unzip ovpn-ro-bucharest.zip && mkdir -p /etc/openvpn && mv config/* /etc/openvpn && chmod +x /etc/openvpn/update-resolv-conf && rm -rf config && rm -f ovpn-ro-bucharest.zip
+echo "CHANGE TO YOUR USERNAME" >> /etc/openvpn/credentials
+echo "CHANGE TO YOUR PASSWORD" >> /etc/openvpn/credentials
+openvpn --config /etc/openvpn/ovpn.conf --daemon
